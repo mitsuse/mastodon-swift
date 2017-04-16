bb804b73eb6f0b6e126c403d550323827fe3578a
Tomoya Kose <tomoya@mitsuse.jp>
2017-04-16 17:27:58 +0900

    `Configuration` provide the id and secret of client.

--- Sources/Client.swift
+++ Sources/Client.swift
@@ -1,2 +1,15 @@
 public protocol Client {
+    var configuration: Configuration { get }
+}
+
+public struct Configuration {
+    public let server: String
+    public let clientId: String
+    public let clientSecret: String
+
+    public init(server: String, clientId: String, clientSecret: String) {
+        self.server = server
+        self.clientId = clientId
+        self.clientSecret = clientSecret
+    }
 }
--- Sources/Client_Default.swift
+++ Sources/Client_Default.swift
@@ -1,2 +1,7 @@
 public struct DefaultClient: Client {
+    public let configuration: Configuration
+
+    public init(configuration: Configuration) {
+        self.configuration = configuration
+    }
 }
