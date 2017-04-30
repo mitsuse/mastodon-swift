8faac653664998a9c49d04edf9b5ffee58e36410
Tomoya Kose <tomoya@mitsuse.jp>
2017-04-30 21:20:33 +0900

    Pass the configuration with each request. The instance to which client sends requests can be changed.

--- Sources/Client.swift
+++ Sources/Client.swift
@@ -1,20 +1,18 @@
 import PromiseKit
 
 public protocol Client {
-    var configuration: Configuration { get }
-
-    func postOAuthToken(userName: String, password: String, scope: String) -> Promise<OAuthTokenJson>
-    func getAccounts(accessToken: String, id: Int) -> Promise<AccountJson>
-    func verifyCredentials(accessToken: String) -> Promise<AccountJson>
-    func followAccounts(accessToken: String, id: Int) -> Promise<RelationshipJson>
-    func unfollowAccounts(accessToken: String, id: Int) -> Promise<RelationshipJson>
-    func blockAccounts(accessToken: String, id: Int) -> Promise<RelationshipJson>
-    func unblockAccounts(accessToken: String, id: Int) -> Promise<RelationshipJson>
-    func muteAccounts(accessToken: String, id: Int) -> Promise<RelationshipJson>
-    func unmuteAccounts(accessToken: String, id: Int) -> Promise<RelationshipJson>
-    func postStatuses(accessToken: String, status: String, inReplyToId: Int?, sensitive: Bool?, spoilerText: String?, visibility: VisibilityJson?) -> Promise<StatusJson>
-    func deleteStatuses(accessToken: String, id: Int) -> Promise<Void>
-    func getTimelinesHome(accessToken: String, maxId: Int?, sinceId: Int?) -> Promise<[StatusJson]>
+    func postOAuthToken(configuration: Configuration, userName: String, password: String, scope: String) -> Promise<OAuthTokenJson>
+    func getAccounts(configuration: Configuration, accessToken: String, id: Int) -> Promise<AccountJson>
+    func verifyCredentials(configuration: Configuration, accessToken: String) -> Promise<AccountJson>
+    func followAccounts(configuration: Configuration, accessToken: String, id: Int) -> Promise<RelationshipJson>
+    func unfollowAccounts(configuration: Configuration, accessToken: String, id: Int) -> Promise<RelationshipJson>
+    func blockAccounts(configuration: Configuration, accessToken: String, id: Int) -> Promise<RelationshipJson>
+    func unblockAccounts(configuration: Configuration, accessToken: String, id: Int) -> Promise<RelationshipJson>
+    func muteAccounts(configuration: Configuration, accessToken: String, id: Int) -> Promise<RelationshipJson>
+    func unmuteAccounts(configuration: Configuration, accessToken: String, id: Int) -> Promise<RelationshipJson>
+    func postStatuses(configuration: Configuration, accessToken: String, status: String, inReplyToId: Int?, sensitive: Bool?, spoilerText: String?, visibility: VisibilityJson?) -> Promise<StatusJson>
+    func deleteStatuses(configuration: Configuration, accessToken: String, id: Int) -> Promise<Void>
+    func getTimelinesHome(configuration: Configuration, accessToken: String, maxId: Int?, sinceId: Int?) -> Promise<[StatusJson]>
 }
 
 public struct Configuration {
